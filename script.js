781f8695e74cd5389ba45c1d8816f4ada365d929
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-12-08 01:33:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ async function fetchPosts() {
     return posts;
 }
 
-function parsePost(post, year) {
+function parsePost(post, postYear) {
     const date = post.match(/\d{2}-\d{2}/)[0];
-    const fullDate = `${year}-${date}`;
+    const fullDate = `${postYear}-${date}`;
+    console.log(fullDate);
     const title = post.match(/^# (.+)$/m)[1];
     const content = post.replace(date, '').replace(/^# .+$/m, '').trim();
 
