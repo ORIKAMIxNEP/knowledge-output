71b8f699dd4921b4b1abeaf730f57e642c9f4362
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-12-07 23:29:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ async function fetchMarkdown(file) {
     const response = await fetch(file);
     const text = await response.text();
 
+    // 空行2つで分割
+    const sections = text.split(/\n\n+/).map(section => section.trim()).filter(Boolean);
     const posts = [];
 
     for (const section of sections) {
